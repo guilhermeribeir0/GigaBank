0144743dda6b1a8f47cea656f110b41a071a70c6
guilhermeribeir0 <dev.guilhermeribeiro@gmail.com>
2023-07-02 10:45:26 -0300

    adiciona base backend ao projeto

---
+++ backendGigaBank/src/main/java/br/com/guilhermeRibeiro/backendGigaBank/GigaBankApplication.java
@@ -0,0 +1,13 @@
+package br.com.guilhermeRibeiro.backendGigaBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GigaBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GigaBankApplication.class, args);
+	}
+
+}
---
+++ backendGigaBank/src/test/java/br/com/guilhermeRibeiro/backendGigaBank/GigaBankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guilhermeRibeiro.backendGigaBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GigaBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

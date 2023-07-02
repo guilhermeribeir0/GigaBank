2f9e5245b36454cad5fe86318bb79ebc7c6d4660
guilhermeribeir0 <dev.guilhermeribeiro@gmail.com>
2023-07-02 11:28:35 -0300

    adiciona repository das entidades

---
+++ backendGigaBank/src/main/java/br/com/guilhermeRibeiro/backendGigaBank/repository/ClienteRepository.java
@@ -0,0 +1,10 @@
+package br.com.guilhermeRibeiro.backendGigaBank.repository;
+
+import br.com.guilhermeRibeiro.backendGigaBank.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+}
---
+++ backendGigaBank/src/main/java/br/com/guilhermeRibeiro/backendGigaBank/repository/ContaBancariaRepository.java
@@ -0,0 +1,10 @@
+package br.com.guilhermeRibeiro.backendGigaBank.repository;
+
+import br.com.guilhermeRibeiro.backendGigaBank.entity.ContaBancaria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContaBancariaRepository extends JpaRepository<ContaBancaria, Long> {
+
+}

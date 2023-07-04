c94b742c9974b19101f880570cdc5699d8c0f3be
guilhermeribeir0 <dev.guilhermeribeiro@gmail.com>
2023-07-03 21:26:37 -0300

    inicia implementacao service conta bancaria

---
+++ backendGigaBank/src/main/java/br/com/guilhermeRibeiro/backendGigaBank/service/ContaBancariaService.java
@@ -0,0 +1,10 @@
+package br.com.guilhermeRibeiro.backendGigaBank.service;
+
+import br.com.guilhermeRibeiro.backendGigaBank.repository.ContaBancariaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContaBancariaService {
+
+    private ContaBancariaRepository contaBancariaRepository;
+}

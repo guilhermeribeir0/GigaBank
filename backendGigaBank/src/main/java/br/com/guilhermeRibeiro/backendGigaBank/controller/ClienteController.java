f6792ecea37bdb72025db570f94cf3b786250194
guilhermeribeir0 <dev.guilhermeribeiro@gmail.com>
2023-07-06 21:35:04 -0300

    ajusta classe clienteController

--- backendGigaBank/src/main/java/br/com/guilhermeRibeiro/backendGigaBank/controller/ClienteController.java
+++ backendGigaBank/src/main/java/br/com/guilhermeRibeiro/backendGigaBank/controller/ClienteController.java
@@ -2,7 +2,6 @@ package br.com.guilhermeRibeiro.backendGigaBank.controller;
 
 import br.com.guilhermeRibeiro.backendGigaBank.dto.ClienteDTO;
 import br.com.guilhermeRibeiro.backendGigaBank.entity.Cliente;
-import br.com.guilhermeRibeiro.backendGigaBank.exception.RegraDeNegocioException;
 import br.com.guilhermeRibeiro.backendGigaBank.service.ClienteService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -18,50 +17,31 @@ public class ClienteController {
     @ResponseStatus(HttpStatus.OK)
     @GetMapping(value = "cliente/{cpf}")
     public @ResponseBody Cliente buscaPorCpf(@PathVariable String cpf) {
-        try {
-            return clienteService.buscarClientePorCpf(cpf);
-        } catch (Exception e) {
-            throw new RegraDeNegocioException("Ocorreu um erro ao executar a requisição. " + e.getMessage());
-        }
+        return clienteService.buscarClientePorCpf(cpf);
     }
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping(value = "cliente/{nome}")
     public @ResponseBody List<Cliente> buscaPorNome(@PathVariable String nome) {
-        try {
-            return clienteService.buscarClientePorNome(nome);
-        } catch (Exception e) {
-            throw new RegraDeNegocioException("Ocorreu um erro ao executar a requisição. " + e.getMessage());
-        }
+        return clienteService.buscarClientePorNome(nome);
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(value = "cadastrar")
     public @ResponseBody Cliente cadastrar(@RequestBody Cliente cliente) {
-        try {
-            return clienteService.cadastrarCliente(cliente);
-        } catch (Exception e) {
-            throw new RegraDeNegocioException("Ocorreu um erro ao executar a requisição. " + e.getMessage());
-        }
+        return clienteService.cadastrarCliente(cliente);
     }
 
     @ResponseStatus(HttpStatus.OK)
     @PutMapping(value = "atualizar/{cpf}")
     public @ResponseBody Cliente atualizarCadastro(@PathVariable String cpf, @RequestBody ClienteDTO clienteDTO) {
-        try {
-            return clienteService.atualizarCadastroCliente(cpf, clienteDTO);
-        } catch (Exception e) {
-            throw new RegraDeNegocioException("Ocorreu um erro ao executar a requisição. " + e.getMessage());
-        }
+        return clienteService.atualizarCadastroCliente(cpf, clienteDTO);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping(value = "/{cpf}")
     public @ResponseBody String deletarCadastro(@PathVariable String cpf) {
-        try {
-            return clienteService.deletarCadastroCliente(cpf);
-        } catch (Exception e) {
-            throw new RegraDeNegocioException("Ocorreu um erro ao executar a requisição. " + e.getMessage());
-        }
+        return clienteService.deletarCadastroCliente(cpf);
+
     }
 }
